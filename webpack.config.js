eee0a22a8316fd8c79057dc20183f1a9b92b106d
Levente Balogh <balogh.levente.hu@gmail.com>
2017-05-15 08:19:24 +0200

    Webpack: add webpack config file

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const { resolve } = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+    context: resolve(__dirname, 'src'),
+
+    entry: [
+        'react-hot-loader/patch',
+        'webpack-dev-server/client?http://localhost:8080',
+        'webpack/hot/only-dev-server',
+        resolve(__dirname, 'src/app/client.js'), // entry point
+    ],
+
+    output: {
+        filename: 'bundle.js',
+        path: resolve(__dirname, 'build'),
+        publicPath: '/'
+    },
+
+    devtool: 'inline-source-map',
+
+    devServer: {
+        hot: true,
+        contentBase: resolve(__dirname, 'build'),
+        publicPath: '/'
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                use: [ 'babel-loader', ],
+                exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                use: [ 'style-loader', 'css-loader?modules', ],
+            },
+        ],
+    },
+
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NamedModulesPlugin(),
+    ],
+};
