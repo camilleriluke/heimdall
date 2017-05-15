5a1368b3c8f5bf0a20ea972ffe54a4197b348398
Levente Balogh <balogh.levente.hu@gmail.com>
2017-05-15 08:20:03 +0200

    App: add a basic skeleton

---
+++ src/app/components/App/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './App.css';
+
+export default function App () {
+    return (
+        <div className={styles.app}>
+            <h2>Hello, </h2>
+        </div>
+    );
+};
