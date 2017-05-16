40ddfc1e4ceb1de291f3c793f3bd45bfe20668a5
Levente Balogh <balogh.levente.hu@gmail.com>
2017-05-16 23:22:38 +0200

    App: rename entrypoint

---
+++ src/app/app.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Routes from './routes';
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import reducers from './reducers';
+
+let store = createStore(reducers);
+
+ReactDOM.render(
+    <Provider store={ store }>
+        <Routes></Routes>
+    </Provider>,
+    document.getElementById('root')
+);
--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
         'react-hot-loader/patch',
         'webpack-dev-server/client?http://localhost:8080',
         'webpack/hot/only-dev-server',
-        resolve(__dirname, 'src/app/client.js'), // entry point
+        resolve(__dirname, 'src/app/app.js'),
     ],
 
     output: {
