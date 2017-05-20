6c3cf5ff7aa596c08b17ece40ae7a0fa6bbdb217
Levente Balogh <balogh.levente.hu@gmail.com>
2017-05-20 09:59:30 +0200

    Unlock: add Title and Next button

--- src/app/pages/Unlock/Unlock.js
+++ src/app/pages/Unlock/Unlock.js
@@ -4,12 +4,15 @@ import generalStyles from '../../styles/general.css';
 import PasswordInput from '../../components/PasswordInput';
 import Row from '../../components/Row';
 import CenteredPage from '../../components/CenteredPage';
+import Title from '../../components/Title';
+import Button from '../../components/Button';
 
 export default function Unlock () {
     return (
         <CenteredPage>
-            Unlooock
+            <Title text='Unlock' />
             <PasswordInput />
+            <Button text='Next' color='green' className={ generalStyles.marginTop } />
         </CenteredPage>
     );
 }
