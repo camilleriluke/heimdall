fb8d485dcbb3fe10d2205f309af90afb60dfd079
Levente Balogh <balogh.levente.hu@gmail.com>
2017-05-27 22:12:21 +0200

    App: add initial state to the app

--- src/app/app.js
+++ src/app/app.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import Routes from './routes';
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
+import ReactDOM from 'react-dom';
+import Routes from './routes';
 import reducers from './reducers';
+import { doesStoreExist, readRawStore } from './utils';
 
-let store = createStore(reducers);
+const initialState = {
+    status: {
+        locked: true,
+        doesStoreExist: doesStoreExist()
+    }
+};
+
+let store = createStore(reducers, initialState);
 
 ReactDOM.render(
     <Provider store={ store }>
