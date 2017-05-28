35c76346e8fe281c5745a0e2de28c5be11997fb5
Levente Balogh <balogh.levente.hu@gmail.com>
2017-05-28 12:33:17 +0200

    App: save items to file on every store-change

--- src/app/app.js
+++ src/app/app.js
@@ -1,4 +1,6 @@
+import fs from 'fs';
 import React from 'react';
+import _ from 'lodash';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
@@ -6,7 +8,7 @@ import thunk from 'redux-thunk';
 import ReactDOM from 'react-dom';
 import Routes from './routes';
 import reducers from './reducers';
-import { doesStoreExist, readRawStore } from './utils';
+import { doesStoreExist, readRawStore, encryptStore } from './utils';
 
 const initialState = {
     status: {
@@ -21,9 +23,38 @@ let store = createStore(
     applyMiddleware(thunk),
 );
 
+
+// DEVELOPMENT --------------
+window.store = store;
+window.store.get = (key) => {
+    const state = window.store.getState();
+
+    return _.get(state, key, 'Not found.');
+};
+//---------------------------
+
+saveAndEncryptOnStoreChange(store);
+
 ReactDOM.render(
     <Provider store={ store }>
         <Routes></Routes>
     </Provider>,
     document.getElementById('root')
 );
+
+function saveAndEncryptOnStoreChange (store) {
+    store.subscribe(() => {
+        const state = store.getState();
+        const items = state.items;
+        const locked = state.status.locked;
+        const password = state.status.password;
+
+        console.log('------------------------------------');
+        console.log('SAVING STORE...');
+        console.log('------------------------------------');
+
+        if (!locked && password) {
+            encryptStore({ items, password });
+        }
+    });
+}
