303afda011741132c2b6c5564d02898faa4b3541
Levente Balogh <balogh.levente.hu@gmail.com>
2017-05-29 20:22:21 +0200

    Webpack: remove unnecessary comma

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,7 @@ module.exports = {
         rules: [
             {
                 test: /\.jsx?$/,
-                use: [ 'babel-loader', ],
+                use: [ 'babel-loader' ],
                 exclude: /node_modules/
             },
             {
