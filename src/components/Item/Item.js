62ec516e9020d3a5c4222c1e62f4945c977a4001
Levente Balogh <balogh.levente.hu@gmail.com>
2017-05-30 23:47:56 +0200

    Item: add copy-to-clipboard for passwords

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -1,11 +1,47 @@
 import React from 'react';
+import { clipboard } from 'electron';
 import './Item.scss';
 
-export default function Item ({ item }) {
-    return (
-        <div className='item'>
-            <div className='item-name'>{ item.name }</div>
-            <div className='item-copy-password'>Copy Password</div>
-        </div>
-    );
+const COPIED_MESSAGE_TIMEOUT = 1500;
+
+export default class Item extends React.Component {
+    constructor (props) {
+        super(props);
+
+        this.copyToClipBoard = this.copyToClipBoard.bind(this);
+        this.state = {
+            displayCopyText: false
+        };
+    }
+
+    copyToClipBoard (text) {
+        this.setState({ displayCopyText: true });
+        clipboard.writeText(text);
+        setTimeout(() => this.setState({ displayCopyText: false }), COPIED_MESSAGE_TIMEOUT);
+    }
+
+    getCopiedMessage () {
+        if (this.state.displayCopyText) {
+            return <div className='item-copied-message'>Copied.</div>;
+        }
+
+        return null;
+    }
+
+    render () {
+        const item = this.props.item;
+
+        return (
+            <div className='item'>
+                <div className='item-name'>{ item.name }</div>
+                { this.getCopiedMessage() }
+                <div
+                    className='item-copy-password'
+                    onClick={ () => this.copyToClipBoard(item.password) }
+                >
+                    Copy Password
+                </div>
+            </div>
+        );
+    }
 }
