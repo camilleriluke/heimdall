fa2e54d837399ad1c0df46b85da54b704835cd4b
Levente Balogh <balogh.levente.hu@gmail.com>
2017-06-07 20:09:01 +0200

    Components/Redirect: introduce a component for handling redirections

---
+++ src/components/Redirect/Redirect.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { push } from '../../actions';
+
+class Redirect extends React.Component {
+    constructor (props) {
+        super(props);
+    }
+
+    componentDidMount () {
+        if (this.props.path) {
+            this.props.redirect(this.props.path);
+        }
+    }
+
+    render () {
+        return null;
+    }
+}
+
+function mapDispatchToProps (dispatch) {
+    return {
+        redirect: path => dispatch(push(path))
+    };
+}
+
+const ConnectedRedirect = connect(
+    () => ({}),
+    mapDispatchToProps
+)(Redirect);
+
+export default ConnectedRedirect;
