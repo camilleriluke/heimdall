9e22288396aaa3983a97809e75353e29d4bfdf57
Levente Balogh <balogh.levente.hu@gmail.com>
2017-06-12 09:10:28 +0200

    Actions: add actions for persisting the state

---
+++ src/actions/persist.js
@@ -0,0 +1,18 @@
+import _ from 'lodash';
+import persistedStore from '../../lib/store';
+
+export function persistState () {
+    return (dispatch, getState) => {
+        const state = getState();
+        const password = _.get(state, 'status.password', null);
+        const items = _.get(state, 'items', []);
+
+        persistedStore.set('status', { locked: false, password });
+        persistedStore.set('items', items);
+    };
+}
+
+export function clearPersistedState () {
+    persistedStore.remove('status');
+    persistedStore.remove('items');
+}
