9c5ecffa44c26399a678cb8d2554efd70c215914
Levente Balogh <balogh.levente.hu@gmail.com>
2017-06-20 22:28:34 +0200

    Item: add icon to delete button

--- src/pages/Item/Item.js
+++ src/pages/Item/Item.js
@@ -12,6 +12,7 @@ export default function Item ({ item, onSubmit, deleteItem }) {
                     text='Delete'
                     className='header-button'
                     color='red'
+                    icon='ion-close-circled'
                     onClick={ () => deleteItem(item) }
                 />
             </Header>
