a6ed54bf5877ff5988afd6601e5bdf77faca744a
Levente Balogh <balogh.levente.hu@gmail.com>
2017-06-20 21:40:20 +0200

    CopyToClipboard: stop propagation

--- src/components/ItemsListRow/ItemsListRow.js
+++ src/components/ItemsListRow/ItemsListRow.js
@@ -7,7 +7,11 @@ export default function ItemsListRow ({ onClick, onCopy, item, displayCopyText }
         <div onClick={ onClick } className='item-list-row'>
             <div className='item-list-row-name'>{ item.name }</div>
             { getCopyMessage(displayCopyText) }
-            <CopyToClipboard className='item-copy-password' text={ item.password } onClick={ onCopy }>
+            <CopyToClipboard
+                className='item-copy-password'
+                text={ item.password }
+                onClick={ onCopy }
+            >
                 Copy Password
             </CopyToClipboard>
         </div>
