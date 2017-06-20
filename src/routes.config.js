cb028143e37635558a8b261ea467827248b2ecba
Levente Balogh <balogh.levente.hu@gmail.com>
2017-06-20 21:22:38 +0200

    Routes Config: remove, as not used anymore

--- src/routes.config.js
+++
@@ -1,9 +0,0 @@
-const routesConfig = {
-    '/create-item': { title: 'Create Item' },
-    '/items': { title: 'Items' },
-    '/unlock': { title: 'Unlock' },
-    '/init': { title: 'Init' },
-    '/': { title: 'Home' }
-};
-
-export default routesConfig;
